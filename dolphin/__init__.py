983fd3cf0aa13dd44564902cb26b5bdf527b9605
Timothée Darcet <timothee.darcet@gmail.com>
2020-03-03 18:21:28 +0100

    Change timestep_limit to max_episode_steps

--- dolphin/__init__.py
+++ dolphin/__init__.py
@@ -7,7 +7,7 @@ register(
     id='vladfi1/SSBM-v0',
     entry_point='dolphin:simpleSSBMEnv',
     reward_threshold=1,
-    timestep_limit=9999999,
+    max_episode_steps=9999999,
     kwargs=dict(
       cpu=9,
       gui=True,
@@ -22,7 +22,7 @@ register(
     id='vladfi1/SSBM-headless-v0',
     entry_point='dolphin:simpleSSBMEnv',
     reward_threshold=1,
-    timestep_limit=9999999,
+    max_episode_steps=9999999,
     kwargs=dict(
       cpu=9,
       stage='battlefield',
